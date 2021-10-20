9c60c7427d0e593e08f297b08cad04a2f35e70ef
zumioo <bumpzumi@gmail.com>
2021-10-20 21:35:16 +0900

    問題追加

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
-const question = 'だってばよ';
-const answers = [
-    'ナルト',
-    'マダラ',
-    'サスケ',
-    'カカシ'
-];
-const correct = 'ナルト';
+const quiz = [
+    {
+        question: 'だってばよ',
+        answers: [
+            'ナルト',
+            'マダラ',
+            'サスケ',
+            'カカシ'
+        ],
+        correct: 'ナルト'
+    },{
+        question: '...うん',
+        answers: [
+            'デイダラ',
+            'マダラ',
+            'サスケ',
+            'カカシ'
+        ],
+        correct: 'デイダラ'
+    },{
+        question: 'めんどくせえ',
+        answers: [
+            'ナルト',
+            'シカマル',
+            'サスケ',
+            'カカシ'
+        ],
+        correct: 'シカマル'
+    }
+]
 
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
 
 const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length
 
 //クイズの問題文と選択肢を定義
 const setupQuiz = () =>{
-    document.getElementById('js-question').textContent = question;
+    document.getElementById('js-question').textContent = quiz[quizIndex].question;
     let buttonIndex = 0;
     while(buttonIndex < buttonLength){
-        $button[buttonIndex].textContent = answers[buttonIndex];
+        $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
         buttonIndex++;
     };
 };
@@ -24,11 +49,19 @@ setupQuiz();
 
 //ボタンの正誤判定
 const clickHandler = (e) => {
-    if(correct === e.target.textContent){
+    if(quiz[quizIndex].correct === e.target.textContent){
         window.alert('正解');
+        score++;
     } else{
         window.alert('不正解');
-    }
+    };
+
+    quizIndex++;
+    if(quizIndex < quizLength){
+        setupQuiz();
+    }else{
+        window.alert('終了！あなたの正解数は' + score + 'です')
+    };
 };
 
 //ボタンクリック時の正誤判定
